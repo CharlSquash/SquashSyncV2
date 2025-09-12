6a03fca6b4a812acf8f9b2ae4249bf6e0f3e6868
Charl <charl.squash@gmail.com>
2025-09-12 07:58:53 +0200

    add players in school group django admin

--- players/admin.py
+++ players/admin.py
@@ -1,14 +1,42 @@
 # players/admin.py
 from django.contrib import admin
+from django import forms
 from .models import (
     SchoolGroup, Player, CourtSprintRecord, VolleyRecord,
     BackwallDriveRecord, MatchResult
 )
 
+# Custom form to allow managing the reverse M2M relationship with a filter_horizontal-like widget
+class SchoolGroupAdminForm(forms.ModelForm):
+    players = forms.ModelMultipleChoiceField(
+        queryset=Player.objects.all(),
+        required=False,
+        widget=admin.widgets.FilteredSelectMultiple(
+            verbose_name='Players',
+            is_stacked=False
+        )
+    )
+
+    class Meta:
+        model = SchoolGroup
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            self.fields['players'].initial = self.instance.players.all()
+
 @admin.register(SchoolGroup)
 class SchoolGroupAdmin(admin.ModelAdmin):
+    form = SchoolGroupAdminForm
     list_display = ('name', 'description')
     search_fields = ('name',)
+
+    def save_model(self, request, obj, form, change):
+        # Save the SchoolGroup instance first
+        super().save_model(request, obj, form, change)
+        # Now, save the many-to-many relationship for players from the form
+        obj.players.set(form.cleaned_data.get('players'))
 
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
@@ -22,3 +50,4 @@ admin.site.register(CourtSprintRecord)
 admin.site.register(VolleyRecord)
 admin.site.register(BackwallDriveRecord)
 admin.site.register(MatchResult)
+
