679efb780485acae3f27df528ceaa729c6fc65e5
Charl <charl.squash@gmail.com>
2025-12-09 05:53:20 +0200

    Old live session removal

--- live_session/urls.py
+++ live_session/urls.py
@@ -5,7 +5,5 @@ from . import views
 app_name = 'live_session'
 
 urlpatterns = [
-    path('session/<int:session_id>/', views.live_session_display, name='live_session_display'),
-    path('api/session/<int:session_id>/update/', views.live_session_update_api, name='live_session_update_api'),
     path('planner-v2/<int:session_id>/', views.experimental_planner, name='planner_v2'),
 ]
