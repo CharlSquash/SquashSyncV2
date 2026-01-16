faa4e0ba136cebafdf54a77c1029b1dadb069c40
Charl <charl.squash@gmail.com>
2026-01-16 07:18:01 +0200

    Login days changes in settings for solosync login to 90 days

--- coach_project/settings.py
+++ coach_project/settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
